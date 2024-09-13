bdc26d0d98531642fe2998db1716a3b642e3fcaf
Ercan Mert Esen <glitch5807@hotmail.com>
2024-09-13 20:47:00 +0300

    Sprite creator function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include <string>
+#include <assert.h>
+
+sf::Sprite& GetSprite(std::string spriteName)
+{
+    sf::Image image;
+    static sf::Texture texture;
+    sf::Sprite sprite;
+
+    if (!image.loadFromFile("Assets/" + spriteName)) 
+    {
+        assert("Couldn't locate the image.");
+    }
+    if (!texture.loadFromImage(image))
+    {
+        assert("Couldn't load image to texture.");
+    }
+
+    sprite.setTexture(texture);
+
+    return sprite;
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "My Game!");
-    sf::CircleShape shape(10);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition(400, 300);
 
     while (window.isOpen())
     {
@@ -17,7 +36,15 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+
+        sf::Sprite spr;
+        spr = GetSprite("chest.png");
+
+        spr.scale({ 0.5f,0.5f });
+        spr.setOrigin({ 235.0f,235.0f });
+        spr.setPosition({ 400.0f,300.0f });
+
+        window.draw(spr);
         window.display();
     }
 
