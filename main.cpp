47c3be08dcad3826db75321aba5e5f20c694f78e
Rüzgar Koray Genç <koray_genc1@hotmail.com>
2024-09-25 22:49:17 +0300

    filter fix

--- main.cpp
+++ main.cpp
@@ -4,22 +4,23 @@
 
 int main()
 {
-    try {
+    try 
+    {
         Window window({ 800u,600u }, "SEX");
         AssetLoader loader("Assets/");
 
-    Transform tr;
-    tr.SetLocalScale({ 5.0f,5.0f });
-    tr.SetLocalRotation(10.f);
-    tr.SetLocalPosition({ 10.f,10.f });
+        Transform tr;
+        tr.SetLocalScale({ 5.0f,5.0f });
+        tr.SetLocalRotation(10.f);
+        tr.SetLocalPosition({ 10.f,10.f });
 
-    tr.Scale({ 2.0f,2.0f });
-    tr.Rotate(15.0f);
-    tr.Translate({ 7.0f,12.0f });
+        tr.Scale({ 2.0f,2.0f });
+        tr.Rotate(15.0f);
+        tr.Translate({ 7.0f,12.0f });
 
-    sf::CircleShape shape1(50);
-    shape1.setFillColor(sf::Color::Yellow);
-    shape1.setPosition(400, 300);
+        sf::CircleShape shape1(50);
+        shape1.setFillColor(sf::Color::Yellow);
+        shape1.setPosition(400, 300);
 
         sf::Sprite spr;
         sf::Texture texture;
