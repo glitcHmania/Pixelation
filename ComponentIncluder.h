54dab8ea6dd50e1a2df039c51060413639360a2f
Rüzgar Koray Genç <koray_genc1@hotmail.com>
2024-12-04 02:58:19 +0300

    namespacified renderer class

--- ComponentIncluder.h
+++ ComponentIncluder.h
@@ -2,3 +2,4 @@
 #include "Component.h"
 #include "SpriteRenderer.h"
 #include "Transform.h"
+#include "TextRenderer.h"
--- Renderer.cpp
+++ Renderer.cpp
@@ -1,68 +1,88 @@
 #include "Renderer.h"
 
-Renderer::Renderer(sf::VideoMode mode, const std::string& windowName)
+namespace Renderer
 {
-	renderWindow = std::make_unique<sf::RenderWindow>(mode, windowName);
-}
-
-void Renderer::Update()
-{
-	renderWindow->clear();
-
-	//temp, is it really?
-	if(mainCamera)
+	namespace 
 	{
-		mainCamera.Move(Time::GetDeltatime());
-		mainCamera.Scale(Time::GetDeltatime());
+		std::vector<std::shared_ptr<sf::Drawable>> queue;
+		std::vector<Camera> cameras;
+		Camera mainCamera;
+		std::unique_ptr<sf::RenderWindow> renderWindow;
 	}
-	//temp, is it really?
 
-	for (const auto& drawable : queue)
+	void Configure(sf::VideoMode mode, const std::string& windowName)
 	{
-		renderWindow->draw(*drawable);
+		renderWindow = std::make_unique<sf::RenderWindow>(mode, windowName);
 	}
-	renderWindow->display();
-}
 
-void Renderer::ResizeWindow()
-{
-	if (mainCamera)
+	void Renderer::Update()
 	{
-		mainCamera.ResizeWindow();
+		renderWindow->clear();
+
+		//temp, is it really?
+		if (mainCamera)
+		{
+			mainCamera.Move(Time::GetDeltatime());
+			mainCamera.Scale(Time::GetDeltatime());
+		}
+		//temp, is it really?
+
+		for (const auto& drawable : queue)
+		{
+			renderWindow->draw(*drawable);
+		}
+		renderWindow->display();
 	}
-}
 
-sf::RenderWindow& Renderer::GetWindow()
-{
-	return *renderWindow;
-}
-
-void Renderer::RemoveDrawable(int index)
-{
-	if (queue.size() > index)
+	void Renderer::ResizeWindow()
 	{
-		queue.erase(queue.begin() + index);
+		if (mainCamera)
+		{
+			mainCamera.ResizeWindow();
+		}
 	}
-}
 
-void Renderer::CreateCamera()
-{
-	cameras.emplace_back(*renderWindow);
-	if (!mainCamera)
+	sf::RenderWindow& Renderer::GetWindow()
 	{
-		mainCamera = cameras.back();
+		return *renderWindow;
 	}
-}
 
-void Renderer::RemoveCamera(int index)
-{
-	if (cameras.size() > index && cameras.at(index) != mainCamera)
+	void Renderer::AddDrawable(const std::shared_ptr<Renderable>& renderable)
 	{
-		cameras.erase(cameras.begin() + index);
+		queue.push_back(renderable);
+	}
+
+	void Renderer::RemoveDrawable(int index)
+	{
+		if (queue.size() > index)
+		{
+			queue.erase(queue.begin() + index);
+		}
+	}
+
+	void Renderer::CreateCamera()
+	{
+		cameras.emplace_back(*renderWindow);
+		if (!mainCamera)
+		{
+			mainCamera = cameras.back();
+		}
+	}
+
+	void Renderer::RemoveCamera(int index)
+	{
+		if (cameras.size() > index && cameras.at(index) != mainCamera)
+		{
+			cameras.erase(cameras.begin() + index);
+		}
+	}
+
+	void Renderer::SetMainCamera(const Camera& camera)
+	{
+		mainCamera = camera;
 	}
 }
 
-void Renderer::SetMainCamera(const Camera& camera)
-{
-	mainCamera = camera;
-}
+
+
+
